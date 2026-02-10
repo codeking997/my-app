b1d46b72a21046162bbb2bbd75d1fcc27e806d04
codeking997 <york@live.no>
2026-02-10 14:52:07 +0100

    final commit set up of component

---
+++ app/helloWorld.tsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+export default function HelloWorld() {
+    return(
+        <div>hi
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/Wolfgang-amadeus-mozart_1.jpg" alt="Mozart" />        
+        </div>
+        
+    )
+}
